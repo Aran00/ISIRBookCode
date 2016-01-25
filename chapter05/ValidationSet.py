264b3f842c2e308b8019d5602f112db0ae9c41b7
Aran <yuran00@gmail.com>
2016-01-26 01:23:30 +0800

    Add the case of R sample

--- chapter05/ValidationSet.py
+++ chapter05/ValidationSet.py
@@ -5,21 +5,36 @@ import pandas as pd
 from numpy import random as nprand
 from sklearn import linear_model
 from islrtools import poly
+''' Under mac, set where the R library is '''
+from sys import platform as _platform
+if _platform == 'darwin':
+    import os
+    os.environ["R_HOME"] = '/Library/Frameworks/R.framework/Versions/3.2/Resources'
+import rpy2.robjects as robjects
 
 
 class ValidationSet:
-    def __init__(self, seed=1):
-        auto = pd.read_csv("../../ISIRExerciseCode/dataset/Auto.csv", na_values=['?'])
-        self.df = auto.dropna()
+    def __init__(self, seed=1, use_R_sample=False):
+        self.df = pd.read_csv("../../ISIRExerciseCode/dataset/Auto.csv", index_col=0)
         # print self.df.shape, self.df.columns
         self.y_col = 'mpg'
         self.x_cols = ['horsepower']
-        nprand.seed(seed)
-        ''' Beware that the first param is not 392 '''
-        train = nprand.choice(self.df.index.values, 196, replace=False)
+
+        if use_R_sample:
+            ''' See details in chapter03 - Exec12 '''
+            data = robjects.r("""
+                set.seed(1)
+                x <- sample(392, 196)
+            """)
+            # Need to substract 1 here
+            train = self.df.iloc[np.array(data) - 1].index.values
+        else:
+            nprand.seed(seed)
+            ''' Beware that the first param is not 392 '''
+            train = nprand.choice(self.df.index.values, 196, replace=False)
+
         self.train_set = self.df.ix[train, :]
         test = np.setdiff1d(self.df.index.values, train)
-        print len(test)
         self.test_set = self.df.ix[test, :]
 
     def poly_1(self):
@@ -35,7 +50,7 @@ class ValidationSet:
         print np.mean(np.square(self.test_set[self.y_col] - predict_values))
 
 if __name__ == '__main__':
-    vs = ValidationSet(2)
+    vs = ValidationSet(use_R_sample=True)
     vs.poly_1()
     vs.poly_i(2)
     vs.poly_i(3)
