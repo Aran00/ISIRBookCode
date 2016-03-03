9c69c3fd2cb4d5da49cdc84009252ab32892b38d
Aran <yuran00@gmail.com>
2016-03-03 08:42:42 +0800

    Add Regression Tree

--- chapter08/DecisionTree.py
+++ chapter08/DecisionTree.py
@@ -1,14 +1,14 @@
 __author__ = 'Aran'
 
 from islrtools import datasets
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.grid_search import GridSearchCV, RandomizedSearchCV
 from sklearn import tree
 from sklearn.externals.six import StringIO
-from sklearn.datasets import load_iris
 from numpy import random
 from time import time
 from operator import itemgetter
+from sklearn.metrics import confusion_matrix, classification_report
 import numpy as np
 import pydot
 
@@ -18,71 +18,77 @@ The decision tree in scikit-learn doesn't support pruning
 So we only can set min sample number in leaf or maximum depth(I think these 2 have similar effect) to avoid overfit
 '''
 class DecisionTree:
-    def __init__(self):
-        self.target_feature = "Sales"
-        data = datasets.load_data("Carseats", self.target_feature, index_col=0, na_values=['NA'])
-        self.hitter, self.feature_names = data.full, data.feature_names
-        self.hitter[self.target_feature] = self.hitter[self.target_feature].map(lambda x: 1 if x > 8 else 0)
+    def __init__(self, classification=True):
+        self.classification = classification
+        self.target_feature = "Sales" if classification else "medv"
+        data = datasets.load_data("Carseats" if classification else "Boston",
+                                  self.target_feature, index_col=0, na_values=['NA'])
+        self.data, self.feature_names = data.full, data.feature_names
+        self.data[self.target_feature] = self.data[self.target_feature].map(lambda x: 1 if x > 8 else 0)
 
     def split_data(self):
         np.random.seed(1)
-        train_index = random.choice(self.hitter.index, 200, replace=False)
+        train_index = random.choice(self.data.index, 200, replace=False)
         # print train_index
-        train_cond = self.hitter.index.isin(train_index)
-        return self.hitter.ix[train_cond], self.hitter.ix[~train_cond]
+        train_cond = self.data.index.isin(train_index)
+        return self.data.ix[train_cond], self.data.ix[~train_cond]
 
     ''' Use cv to choose parameters '''
-    def choose_params(self):
+    def choose_params(self, random_search=False):
         train, test = self.split_data()
         X, y = train[self.feature_names], train[self.target_feature]
-        dt = DecisionTreeClassifier()
+        dt = DecisionTreeClassifier() if self.classification else DecisionTreeRegressor()
         param_dist = {
             # "criterion": ["gini", "entropy"],
-            "max_features": np.arange(1, 9),
-            "max_depth": np.arange(2, 16)
+            "max_features": np.arange(1, len(self.feature_names) + 1),
+            "max_leaf_nodes": np.arange(2, 20)
         }
-        n_iter_search = 20
-        random_search = RandomizedSearchCV(dt, param_distributions=param_dist, n_iter=n_iter_search)
-        for i in xrange(10):
-            np.random.seed(i)
-            start = time()
-            random_search.fit(X, y)
-            print("RandomizedSearchCV took %.2f seconds for %d candidates"
-                  " parameter settings." % ((time() - start), n_iter_search))
-            DecisionTree.report(random_search.grid_scores_, n_top=1)
-            top_score = sorted(random_search.grid_scores_, key=itemgetter(1), reverse=True)[0]
-            print "Randomized search choose best:", top_score.parameters
 
-        # run grid search
         np.random.seed(1)
-        grid_search = GridSearchCV(dt, param_grid=param_dist)
+        search = RandomizedSearchCV(dt, param_distributions=param_dist, n_iter=20) \
+            if random_search else GridSearchCV(dt, param_grid=param_dist)
         start = time()
-        grid_search.fit(X, y)
-        print("\n\nGridSearchCV took %.2f seconds for %d candidate parameter settings."
-              % (time() - start, len(grid_search.grid_scores_)))
-        DecisionTree.report(grid_search.grid_scores_, n_top=3)
+        search.fit(X, y)
+        print("RandomizedSearchCV took %.2f seconds for %d candidates"
+              " parameter settings." % ((time() - start), 20))
+        DecisionTree.report(search.grid_scores_, n_top=1)
+        top_score = sorted(search.grid_scores_, key=itemgetter(1), reverse=True)[0]
+        print "Randomized search choose best:", top_score.parameters
+        best_param = search.best_params_
+        print best_param
+        best_estimator = search.best_estimator_
 
+        # Print test data confusion matrix
+        y_true = test[self.target_feature]
+        y_pred = best_estimator.predict(test[self.feature_names])
+        if self.classification:
+            cm = confusion_matrix(y_true, y_pred)
+            print cm
+            print classification_report(y_true, y_pred)
+        else:
+            print "The MSE is:", np.mean(np.square(y_true - y_pred))
+        self.output_final_tree(**best_param)
 
-    ''' An overfit tree '''
-    def test_decision_tree(self):
-        dt = DecisionTreeClassifier()
-        X = self.hitter[self.feature_names]
-        y = self.hitter[self.target_feature]
+    ''' Use the params from train set cv and all data to train a new tree '''
+    def output_final_tree(self, **param):
+        init_func = DecisionTreeClassifier if self.classification else DecisionTreeRegressor
+        dt = init_func(**param)
+        X = self.data[self.feature_names]
+        y = self.data[self.target_feature]
         dt.fit(X, y)
         preds = dt.predict(X)
-        print dt, "\n", dt.n_features_
-        print (y == preds).mean()
+        if self.classification:
+            print "The final train accuracy is:", (y == preds).mean()   # test accuracy
+        else:
+            print "The final train MSE is:", np.mean(np.square(y - preds))
         self.print_decision_tree(dt)
         self.print_detail(dt)
 
     def print_decision_tree(self, dt):
-        clf = tree.DecisionTreeClassifier()
-        iris = load_iris()
-        dt = clf.fit(iris.data, iris.target)
         str_buffer = StringIO()
-        tree.export_graphviz(dt, out_file=str_buffer)
+        tree.export_graphviz(dt, out_file=str_buffer, feature_names=self.feature_names)
         graph = pydot.graph_from_dot_data(str_buffer.getvalue())
-        graph.write_pdf("myfile.pdf")
+        graph.write_pdf("output/%s.pdf" % self.target_feature)
 
     def print_detail(self, dt):
         print dt.classes_, dt.feature_importances_, dt.max_features_, dt.n_features_, dt.tree_.node_count
@@ -96,9 +102,8 @@ class DecisionTree:
                   score.mean_validation_score,
                   np.std(score.cv_validation_scores)))
             print("Parameters: {0}".format(score.parameters))
-            print("")
+
 
 if __name__ == '__main__':
     dt_test = DecisionTree()
-    # dt_test.test_decision_tree()
-    dt_test.choose_params()
+    dt_test.choose_params(True)
