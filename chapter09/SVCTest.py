429e657bce4d79f013e5b8520e2288777ec2e85c
ryu <ryu@microstrategy.com>
2016-03-09 18:20:56 +0800

    Add chapter 9 example

---
+++ chapter09/SVCTest.py
@@ -0,0 +1,63 @@
+__author__ = 'ryu'
+
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import confusion_matrix, classification_report
+from examples.plot_separating_hyperplane import plot_svc
+
+
+class SVCTest:
+    def __init__(self):
+        self.init_data()
+
+    def init_data(self):
+        np.random.seed(3)
+        # X = np.random.randn(20, 2)
+        N = 10
+        ''' train data '''
+        self.X = np.random.standard_normal(size=(2*N, 2))
+        # y = np.hstack((-np.ones(N), np.ones(N)))
+        self.y = np.array([-1]*N + [1]*N)
+        self.X[self.y == 1] += 1
+        ''' test data '''
+        self.test_X = np.random.standard_normal(size=(2*N, 2))
+        self.test_y = np.random.choice([-1, 1], 2*N)
+        self.test_X[self.test_y == 1] += 1
+
+    def svc_plot_test(self, clf):
+        ''' cmap is used when c is integer '''
+        '''
+        c = map(lambda z: 'g' if z == 1 else 'r', y)
+        plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired)
+        plt.show()
+        '''
+        # print clf
+        # clf.fit(self.X, self.y)
+        # self.print_svc_detail(clf)
+        plot_svc(clf, self.X, self.y)
+
+    def print_svc_detail(self, clf):
+        print clf.coef_, "\n", clf.intercept_, "\n", clf.support_, "\n", clf.support_vectors_
+
+    def svc_cv_test(self):
+        # parameters = {'C': [0.001, 0.01, 0.1, 1, 5, 10, 100]}
+        parameters = {'C': np.logspace(-3, 2, 6)}
+        svr = SVC(kernel="linear")
+        search = GridSearchCV(svr, parameters, cv=10)
+        search.fit(self.X, self.y)
+        print search.best_params_, "\n", search.best_score_
+        return search.best_estimator_
+
+    def test_set_verify(self, clf):
+        y_pred = clf.predict(self.test_X)
+        print y_pred, "\n", self.test_y
+        cm = confusion_matrix(self.test_y, y_pred)
+        print cm
+        print classification_report(self.test_y, y_pred)
+
+if __name__ == '__main__':
+    st = SVCTest()
+    best_model = st.svc_cv_test()
+    st.test_set_verify(best_model)
+    plot_svc(best_model, st.X, st.y)
