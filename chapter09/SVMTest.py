25f1c235c620bc5517bb9ca97e753bc70fd2f34a
Aran <yuran00@gmail.com>
2016-03-11 01:37:37 +0800

    Add a bit code

--- chapter09/SVMTest.py
+++ chapter09/SVMTest.py
@@ -1,13 +1,14 @@
 __author__ = 'ryu'
 
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn.grid_search import GridSearchCV
 from sklearn.metrics import confusion_matrix, classification_report
 from examples.plot_separating_hyperplane import plot_svc
 
 
-class SVCTest:
+class SVMTest:
     def __init__(self, rand_seed=1):
         np.random.seed(1)
         n = 100
@@ -17,13 +18,24 @@ class SVCTest:
         self.y = np.array([1]*(3*n/2) + [2]*(n/2))
         print self.y
         ''' train data '''
-        train_index = np.random.choice(2*n, n)
-        np.zeros((), )
+        train_index_raw = np.random.choice(2*n, n)
+        train_index = np.zeros(2*n, dtype=np.bool)
+        train_index[train_index_raw] = 1
+
         self.train_X = self.X[train_index]
         self.train_y = self.y[train_index]
 
         ''' test data '''
-        self.test_X = np.random.standard_normal(size=(2*n, 2))
-        self.test_y = np.random.choice([-1, 1], 2*n)
+        self.test_X = self.X[~train_index]
+        self.test_y = self.y[~train_index]
         self.test_X[self.test_y == 1] += 1
 
+    def scatter_x(self):
+        """ cmap is used when c is integer """
+        # c = map(lambda z: 'g' if z == 1 else 'r', self.y)
+        plt.scatter(self.X[:, 0], self.X[:, 1], c=self.y, cmap=plt.cm.Paired)
+        plt.show()
+
+if __name__ == '__main__':
+    st = SVMTest()
+    st.scatter_x()
